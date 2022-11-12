b58826b84457927e4ed34ea4609ebab4447ac175
GustavoSDS <simondossantos18@gmail.com>
2022-11-12 20:04:16 -0300

    desafio completo

--- 09_codigo_morse/ejercicio/js/index.js
+++ 09_codigo_morse/ejercicio/js/index.js
@@ -3,3 +3,10 @@
 
 const input = document.getElementById('input');
 const resultados = document.getElementById('resultados');
+
+input.addEventListener('input', ()=>{
+    resultados.innerText = '';
+    const VALOR = input.value.toLowerCase().split('');
+    const valorTraducido = VALOR.map(letra => `${CODIGO_MORSE[letra]}` ?? letra).join('');
+    resultados.innerText = valorTraducido;
+});
