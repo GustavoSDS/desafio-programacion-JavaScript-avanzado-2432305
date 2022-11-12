da290e1ce9e8e8be282e60132b17275d3afd0519
Gustavo S. Dos Santos <54504583+GustavoSDS@users.noreply.github.com>
2022-11-12 00:02:13 +0000

    Tarea completa

--- 02_objetos_iguales/ejercicio/js/index.js
+++ 02_objetos_iguales/ejercicio/js/index.js
@@ -2,7 +2,24 @@
 // Escribir una función que determine si dos objetos son iguales:
 
 function sonIguales(obj1, obj2) {
-    
+    if(Array.isArray(obj1) && Array.isArray(obj2)){
+        for (let i = 0; i <= obj1.length; i++) {
+            if (!sonIguales(obj1[i], obj2[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+    if (typeof obj1 === 'object' && typeof obj2 === 'object') {
+        const keys = Object.keys(obj1);
+        for (const key of keys) {
+            if (!sonIguales(obj1[key], obj2[key])) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return obj1 === obj2;
 }
 // Todos los console logs deben retornar true:
 console.log(sonIguales(2, 2));
