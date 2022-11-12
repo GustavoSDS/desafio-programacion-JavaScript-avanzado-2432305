cf1b217d39577de538f10a9951355bdbdc55a087
GustavoSDS <simondossantos18@gmail.com>
2022-11-12 10:01:30 -0300

    desafio completo

--- 03_tiempo_restante/ejercicio/js/index.js
+++ 03_tiempo_restante/ejercicio/js/index.js
@@ -9,25 +9,27 @@ const ms_hora = 1000 * 60 * 60;
 const ms_minuto = 1000 * 60;
 const ms_segundo = 1000;
 
-const hoy = new Date();
-let resto;
-
-const diffMilisegundos = Math.abs(fechaFinal - hoy);
-const annos = Math.floor(diffMilisegundos / ms_anno);
-
-resto = diffMilisegundos % ms_anno;
-const meses = Math.floor(resto / ms_mes)
-
-resto = diffMilisegundos % ms_mes;
-const dias = Math.floor(resto / ms_dia)
-
-resto = diffMilisegundos % ms_dia;
-const horas = Math.floor(resto / ms_hora)
-
-resto = diffMilisegundos % ms_hora;
-const minutos = Math.floor(resto / ms_minuto)
-
-resto = diffMilisegundos % ms_minuto;
-const segundos = Math.floor(resto / ms_segundo)
-
-resultado.innerText = `${annos}: ${meses}: ${dias}: ${minutos}: ${segundos}`
+setInterval(()=>{
+    const hoy = new Date();
+    let resto;
+    
+    const diffMilisegundos = Math.abs(fechaFinal - hoy);
+    const annos = Math.floor(diffMilisegundos / ms_anno);
+    
+    resto = diffMilisegundos % ms_anno;
+    const meses = Math.floor(resto / ms_mes)
+    
+    resto = diffMilisegundos % ms_mes;
+    const dias = Math.floor(resto / ms_dia)
+    
+    resto = diffMilisegundos % ms_dia;
+    const horas = Math.floor(resto / ms_hora)
+    
+    resto = diffMilisegundos % ms_hora;
+    const minutos = Math.floor(resto / ms_minuto)
+    
+    resto = diffMilisegundos % ms_minuto;
+    const segundos = Math.floor(resto / ms_segundo)
+    
+    resultado.innerText = `${annos}: ${meses}: ${dias}: ${minutos}: ${segundos}`
+}, ms_segundo);
