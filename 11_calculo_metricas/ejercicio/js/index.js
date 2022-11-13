9410de62bd2286f27432e5aa5e7d56249215becd
GustavoSDS <simondossantos18@gmail.com>
2022-11-12 21:03:27 -0300

    desafio completo

--- 11_calculo_metricas/ejercicio/js/index.js
+++ 11_calculo_metricas/ejercicio/js/index.js
@@ -6,7 +6,34 @@
 const tablaLocalidad = document.getElementById('tabla-localidad');
 const tablaMes = document.getElementById('tabla-mes');
 
+fetch('../metricas.json')
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+        const accuL = {};
+        const accuM = {};
+        for (const index in datos) {
+            const EVAL = datos[index];
+            if (!accuL[EVAL.estado]) {
+                accuL[EVAL.estado] = { acc: 0, contador: 0 };
+            }
+            accuL[EVAL.estado].acc += parseInt(EVAL.evaluacion);
+            accuL[EVAL.estado].contador += 1;
+            const MES = EVAL.fecha.split('-').filter((val, i) => i !== 2).join('-');
 
+            if (!accuM[MES]) {
+                accuM[MES] = { acc: 0, contador: 0 };
+            }
+            accuM[MES].acc += parseInt(EVAL.evaluacion);
+            accuM[MES].contador += 1;
+        }
+
+        for(const key of Object.keys(accuL)){
+            tablaLocalidad.appendChild(generarFila(key, Math.floor(accuL[key].acc / accuL[key].contador)));
+        }
+        for(const key of Object.keys(accuM)){
+            tablaMes.appendChild(generarFila(key, Math.floor(accuM[key].acc / accuM[key].contador)));
+        }
+    });
 
 function generarFila(texto, valor) {
     const tr = document.createElement('tr');
